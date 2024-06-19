45076fa196646bd0d4044a851c01f440da4cfa00
Josh HD <jishhd@gmail.com>
2024-06-18 23:53:10 -0400

    Desktop transition animation tweaks

--- composeApp/src/desktopMain/kotlin/screens/Navigation.desktop.kt
+++ composeApp/src/desktopMain/kotlin/screens/Navigation.desktop.kt
@@ -6,8 +6,8 @@ import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -26,31 +26,31 @@ actual val TRANSITION_OFFSET_DIV = 10
 actual fun ScreenEnterTransition(): EnterTransition =
     fadeIn(
         animationSpec = tween(TRANSITION_ENTER_MS, easing = TRANSITION_EASING)
-    ) + slideInHorizontally(
-        initialOffsetX = { it / TRANSITION_OFFSET_DIV },
+    ) + scaleIn(
+        initialScale = 1f + 1f / TRANSITION_OFFSET_DIV,
         animationSpec = tween(TRANSITION_ENTER_MS, easing = TRANSITION_EASING)
     )
 
 actual fun ScreenExitTransition(): ExitTransition =
     fadeOut(
         animationSpec = tween(TRANSITION_EXIT_MS, easing = TRANSITION_EASING)
-    ) + slideOutHorizontally(
-        targetOffsetX = { -it / TRANSITION_OFFSET_DIV },
+    ) + scaleOut(
+        targetScale = 1f - 1f / TRANSITION_OFFSET_DIV,
         animationSpec = tween(TRANSITION_EXIT_MS, easing = TRANSITION_EASING)
     )
 
 actual fun ScreenPopEnterTransition(): EnterTransition =
     fadeIn(
         animationSpec = tween(TRANSITION_ENTER_MS, easing = TRANSITION_EASING)
-    ) + slideInHorizontally(
-        initialOffsetX = { -it / TRANSITION_OFFSET_DIV },
+    ) + scaleIn(
+        initialScale = 1f - 1f / TRANSITION_OFFSET_DIV,
         animationSpec = tween(TRANSITION_ENTER_MS, easing = TRANSITION_EASING)
     )
 
 actual fun ScreenPopExitTransition(): ExitTransition =
     fadeOut(
         animationSpec = tween(TRANSITION_EXIT_MS, easing = TRANSITION_EASING)
-    ) + slideOutHorizontally(
-        targetOffsetX = { it / TRANSITION_OFFSET_DIV },
+    ) + scaleOut(
+        targetScale = 1f + 1f / TRANSITION_OFFSET_DIV,
         animationSpec = tween(TRANSITION_EXIT_MS, easing = TRANSITION_EASING)
     )
